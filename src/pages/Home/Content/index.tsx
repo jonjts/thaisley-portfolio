9a1be68309ddb3aba4bb3c5555725e33e3ef7653
Jonas <jonjts@gmail.com>
2020-09-20 07:14:50 -0300

    Removido component navegação entre logos e interfaces na home.

--- src/pages/Home/Content/index.tsx
+++ src/pages/Home/Content/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next'
 import { useHistory } from "react-router-dom";
-import { interfaces, logos } from "../../../store";
+import { interfaces  } from "../../../store";
 
 import ContactButtonContainer from '../../../components/container/ContactButton'
 import Card from "../../../components/cards/Image";
@@ -14,43 +14,11 @@ import {
 } from './styles';
 import { S1 } from '../../styles'
 
-import SelectButton from "../../../components/SelectButton";
-
 const Content: React.FC = () => {
 
     const { t } = useTranslation()
     const history = useHistory()
-    const [selected, setSelected] = useState('interfaces')
 
-
-    function focus() {
-        window.location.href = '#jobs'
-    }
-    function handleShowInterfaces() {
-        setSelected('interfaces')
-        focus()
-    }
-
-    function handleShowIndentidades() {
-        setSelected('identidades')
-        focus()
-    }
-
-    const Identidades = () => (
-        <CardRow>
-            {
-                logos.map((logo, key) => (
-                    <CardColumn key={key} xs={12} sm={12} md={12} lg={4}>
-                        <Card
-                            imageSorce={logo.image}
-                            categoria={t(logo.categoria)}
-                            title={logo.title}
-                        />
-                    </CardColumn>
-                ))
-            }
-        </CardRow>
-    )
 
     const Interfaces = () => (
         <CardRow>
@@ -74,24 +42,8 @@ const Content: React.FC = () => {
 
     return (
         <Container id='jobs'>
-            <HeaderContainer>
-                <S1 style={{ marginBottom: 26 }}>
-                    {t('label.show_me')}
-                </S1>
-                <SelectButton
-                    onIdentidadeClicked={handleShowIndentidades}
-                    onInterfacesCliecked={handleShowInterfaces}
-                    onClick={focus}
-                />
-            </HeaderContainer>
-
             <CardContainer>
-                {
-                    selected === 'interfaces' ?
-                        <Interfaces />
-                        :
-                        <Identidades />
-                }
+                <Interfaces />
             </CardContainer>
             <ContactButtonContainer
                 style={{ marginBottom: 113 }}
